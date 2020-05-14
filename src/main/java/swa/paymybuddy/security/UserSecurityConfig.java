cc003c95c8aba9559c96414f5bc48e2fef66b67b
philippegeyaxa <philippe.gey@axa.fr>
2020-05-14 23:57:14 +0200

    persistent login implemented

--- src/main/java/swa/paymybuddy/security/UserSecurityConfig.java
+++ src/main/java/swa/paymybuddy/security/UserSecurityConfig.java
@@ -2,8 +2,6 @@ package swa.paymybuddy.security;
 
 import javax.sql.DataSource;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/test/java/swa/paymybuddy/integration/UserControllerIT.java
+++ src/test/java/swa/paymybuddy/integration/UserControllerIT.java
@@ -4,6 +4,7 @@ import static org.springframework.security.test.web.servlet.request.SecurityMock
 import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
 import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -12,7 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -41,11 +41,25 @@ public class UserControllerIT {
 	}
 	
 	@Test
-	public void givenValidCredentials_whenLogin_thenUserIsAuthenticated() throws Exception
+	public void givenNewUser_whenRegister_thenUserIsSavedInDatabase() throws Exception
 	{
 		// GIVEN
 		String email = "email_1";
-		userRepository.save(new User(1, 0, email, passwordCrypted));
+		String form = "email=" + email + "&password=" + passwordClear;
+		
+		mvc.perform( post("/register")
+				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+		        .content(form)
+		        .accept(MediaType.APPLICATION_FORM_URLENCODED))
+		        .andExpect(status().isCreated());
+	}	
+
+	@Test
+	public void givenValidCredentials_whenLogin_thenUserIsAuthenticated() throws Exception
+	{
+		// GIVEN
+		String email = "email_2";
+		userRepository.save(new User(0, 0, email, passwordCrypted));
 		// WHEN & THEN
 		mvc.perform(formLogin("/login").user(email).password(passwordClear)).andExpect(authenticated());
 	}
@@ -54,25 +68,25 @@ public class UserControllerIT {
 	public void givenWrongCredentials_whenLogin_thenUserIsNotAuthenticated() throws Exception
 	{
 		// GIVEN
-		String email = "email_2";
-		userRepository.save(new User(2, 0, email, "wrong"));
+		String email = "email_3";
+		userRepository.save(new User(0, 0, email, "wrong"));
 		// WHEN & THEN
 		mvc.perform(formLogin("/login").user(email).password(passwordClear)).andExpect(unauthenticated());
 	}
 
 	@Test
-	public void givenNewUser_whenRegister_thenUserIsSavedInDatabase() throws Exception
+	public void givenUserWithPersistenceToken_whenLoginWithoutCredentials_thenUserIsAuthenticated() throws Exception
 	{
 		// GIVEN
-		String email = "email_3";
-		String form = "email=" + email + "&password=" + passwordClear;
-		
-		mvc.perform( MockMvcRequestBuilders
-				.post("/register")
+		String email = "email_4";
+		userRepository.save(new User(0, 0, email, passwordCrypted));
+		String form = "username=" + email + "&password=" + passwordClear + "&remember-me=on";
+		mvc.perform( post("/login")
 				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
 		        .content(form)
-		        .accept(MediaType.APPLICATION_FORM_URLENCODED))
-		        .andExpect(status().isCreated());
+		        .accept(MediaType.APPLICATION_FORM_URLENCODED)
+		).andExpect(authenticated());
+		// WHEN & THEN
+		mvc.perform(formLogin("/login").user(email).password("anything")).andExpect(authenticated());
 	}
-	
 }
